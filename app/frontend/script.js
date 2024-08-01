03d32ab6c8b368d224ea3ba1d8eabd6fee52cc3d
chebupelka8 <stpzamyatin@gmail.com>
2024-08-01 17:59:58 +0500

    Modified frontend

--- app/frontend/script.js
+++ app/frontend/script.js
@@ -1,16 +1,19 @@
 "use strict";
 
-
 import { UserRepository } from "./src/user_repository.js";
 import { HTMLManager } from "./src/html_manager.js";
 
 
 const searchUserInput = document.getElementById("search-users");
+const usersPreview = document.getElementById("users-preview");
 
 document.getElementById("search").addEventListener("click", function() {
+    usersPreview.querySelectorAll(".user").forEach(entry => entry.remove());  // clear usersPreview
+
     if (searchUserInput.value.trim().length > 0) {
         UserRepository.searchUser(searchUserInput.value).then(function(response) {
+            console.log(response["found"]);
             HTMLManager.showReceivedUsers(response["found"]);
         });
     }
-})
+});
